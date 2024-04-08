89b5745da78d1519d66592346ca058384eb97d12
Gandharv Talikoti <gandharvtalikoti19@gmail>
2024-04-08 13:12:57 +0530

    generating answeres in formatted manner

--- practworks.py
+++ practworks.py
@@ -33,4 +33,28 @@ def generate_question_and_answer():
         years} years, the sum of their ages will be {sum_of_ages} years. What is their age now?"
     
     # print(question)
+    # Printing formatted answer
+    answer = f"Since the ages of {name1} and {name2} are in the ratio of {numerator}:{
+        denominator} today, let their ages be {numerator}x and {denominator}x respectively.\n"
+    answer += f"Their individual ages after {years} years will be $ {
+        numerator}x + {years} $ and $ {denominator}x + {years} $.\n"
+    answer += f"It is given that the sum of their ages after {
+        years} years will be {sum_of_ages}. Hence we have,\n"
+    answer += f"\\[ {numerator}x + {years} + {denominator}x + {years} = {sum_of_ages} \\]\n"
+    answer += f"\\[ {numerator + denominator}x + {2 *
+                                                  years} = {sum_of_ages} \\]\n"
+    answer += f"\\[ {numerator +
+                     denominator}x = {sum_of_ages} - {2*years} \\]\n"
+    answer += f"\\[ {numerator + denominator}x = {sum_of_ages - 2*years} \\]\n"
+    answer += f"\\[ x = \\frac{{{sum_of_ages - 2 *
+                                 years}}}{{{numerator + denominator}}} \\]\n"
+    answer += f"\\[ x = {x} \\]\n"
+    answer += f"\nHence the age of {name1} is $ {int(numerator)} \\times {int(x)} = {
+        age1_now} $ and the age of {name2} is $ {int(denominator)} \\times {int(x)} = {age2_now} $."
+    
+    return question, answer
 
+
+question, answer = generate_question_and_answer()
+print("Question: ", question)
+print("Answer: ", answer)
